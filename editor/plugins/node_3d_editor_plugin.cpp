672b9949d14853168ffc90b6c919bd89921cd0bf
kj-art-dev <56623499+kj-art-dev@users.noreply.github.com>
2025-06-08 15:43:23 -0700

    Update node_3d_editor_plugin.cpp
    Added "r:" toggle for the ruler measurement when SHIFT is pressed.
    
    Co-Authored-By: kj-art-dev <56623499+kj-art-dev@users.noreply.github.com>
    Co-Authored-By: Misaka Rin <79098451+MisakaRinOwO@users.noreply.github.com>

--- editor/plugins/node_3d_editor_plugin.cpp
+++ editor/plugins/node_3d_editor_plugin.cpp
@@ -3025,7 +3025,7 @@ void Node3DEditorViewport::_notification(int p_what) {
 
 				// Calculate ruler label position
 				float distance = start_pos.distance_to(end_pos);
-				ruler_label->set_text(TS->format_number(vformat("r: %.3f m", distance)));
+				ruler_label->set_text(TS->format_number(vformat("%.3f m", distance)));
 				Vector3 center = (start_pos + end_pos) / 2;
 				Vector2 screen_position = camera->unproject_position(center) - (ruler_label->get_custom_minimum_size() / 2);
 				ruler_label->set_position(screen_position);
@@ -3034,6 +3034,8 @@ void Node3DEditorViewport::_notification(int p_what) {
 
 				// Add vertices for helper lines
 				if (show_components) {
+					ruler_label->set_text(TS->format_number(vformat("r: %.3f m", distance)));
+
 					// XZ helper line
 					if (abs(start_pos.z - end_pos.z) > 0.0001 && abs(start_pos.x - end_pos.x) > 0.0001) {
 						geometry_x_z->surface_begin(Mesh::PRIMITIVE_LINES);
